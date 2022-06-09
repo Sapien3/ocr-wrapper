f2c4160ddc9512ef7db9353f5757660c49b98f94
bahaa rizk <bahaarizk17@gmail.com>
2022-06-09 11:38:22 +0300

    update

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const { exec } = require("child_process");
-var yourscript = exec("bash test.sh", (error, stdout, stderr) => {
-  console.log(stdout);
-  console.log(stderr);
-  if (error !== null) {
-    console.log(`exec error: ${error}`);
-  }
+const executeScript = (script) => {
+  exec(script, (error, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    }
+  });
+};
+
+const express = require("express");
+const app = express();
+const port = 3070;
+
+app.get("/", (req, res) => {
+  console.log("started excuting...");
+  executeScript("bash pdfOcr.sh");
+  res.send("done");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
 });
